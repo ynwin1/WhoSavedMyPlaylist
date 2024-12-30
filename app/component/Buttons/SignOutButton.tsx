16eb7184927615eabdf559aa430cc9ac13015f02
ynwin1 <ynwinubc24@gmail.com>
2024-12-30 00:05:02 -0800

    log out instead of sign out

--- app/component/Buttons/SignOutButton.tsx
+++ app/component/Buttons/SignOutButton.tsx
@@ -8,7 +8,7 @@ const SignOutButton = () => {
             onClick={() => signOut({callbackUrl: "/"})}
             className="px-6 py-3 bg-red-500 text-white rounded-full font-semibold hover:bg-red-700 transition-colors"
         >
-            Sign Out
+            Log Out
         </button>
     )
 }
